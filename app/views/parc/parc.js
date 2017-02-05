4023409565ef3781197a992eddbe6cb704f6d07c
Konstantin <fedorov.konstantin@gmail.com>
2017-02-05 06:05:57 -0500

    Stylin'

--- app/views/parc/parc.js
+++ app/views/parc/parc.js
@@ -12,28 +12,32 @@ angular.module('myApp.parc', ['ngRoute'])
   .controller('parcCtrl', ['$scope', '$routeParams', function ($scope, $routeParams) {
     $scope.loaded = false;
     $scope.parc = {};
-    var endpoint = "http://sample-env.nwt3za9ckd.us-west-2.elasticbeanstalk.com";
-    $.getJSON("data/allparcs.geojson", function (json) {
-      $scope.parc = json.features.filter(function (feat) { return feat.id === $routeParams.parcId })[0].properties;
-      $scope.loaded = true;
-      ;
 
-      $.getJSON(endpoint + "/twitter/feedbycontent/" + window.encodeURIComponent($scope.parc.name), function (data) {
-        $scope.$apply(function () { $scope.parc.twitter = data.statuses; });
-      });
+    function init() {
+      var endpoint = "http://sample-env.nwt3za9ckd.us-west-2.elasticbeanstalk.com";
+      $.getJSON("data/allparcs.geojson", function (json) {
+        $scope.parc = json.features.filter(function (feat) { return feat.id === $routeParams.parcId })[0].properties;
+        $scope.loaded = true;
 
-      $.getJSON(endpoint + "/facebook/eventsFrom/" + window.encodeURIComponent($scope.parc.fbid), function (data) {
-        $scope.$apply(function () {
-          console.log(data);
-          $scope.parc.events = data.filter(function (event) {
-            return new Date(event.start_time) > new Date()
-          }).sort(function (o1, o2) {
-            var date1 = new Date(o1.start_time);
-            var date2 = new Date(o2.start_time);
-            return date1 < date2  ? -1 : date1 > date2 ? 1 : 0;
-          });;
+          $.getJSON(endpoint + "/twitter/feedbycontent/" + window.encodeURIComponent($scope.parc.name), function (data) {
+            $scope.$apply(function () { $scope.parc.twitter = data.statuses; });
+          });
+
+        $.getJSON(endpoint + "/facebook/eventsFrom/" + window.encodeURIComponent($scope.parc.fbid), function (data) {
+          $scope.$apply(function () {
+            console.log(data);
+            $scope.parc.events = data.filter(function (event) {
+              return new Date(event.start_time) > new Date()
+            }).sort(function (o1, o2) {
+              var date1 = new Date(o1.start_time);
+              var date2 = new Date(o2.start_time);
+              return date1 < date2 ? -1 : date1 > date2 ? 1 : 0;
+            });;
+          });
         });
-      });
 
-    });
+      });
+    }
+
+    init();
   }]);
