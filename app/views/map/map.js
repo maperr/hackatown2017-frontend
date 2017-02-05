5278997cb552e840991f428df0690d267b1a2bd9
Konstantin <fedorov.konstantin@gmail.com>
2017-02-04 22:58:38 -0500

    Progres^

--- app/views/map/map.js
+++ app/views/map/map.js
@@ -9,17 +9,17 @@ angular.module('myApp.map', ['ngRoute'])
     });
   }])
 
-  .controller('mapCtrl', [function () {
+  .controller('mapCtrl', ["$scope", "$compile", function ($scope, $compile) {
+    $scope.parcs = {
+      favs: [],
+      selectedPark: {}
+    }
+    $scope.ids = 0;
+
     var map;
     // called by google maps script
     var lastinfowindow;
     var lastClickedFeature;
-    var infoWindowTemplate =
-      "<div>" +
-      "<h5>%name%</h5>" +
-      "<p><b>ID:</b> %id%" +
-      "</p>" +
-      "</div>";
 
 
     function initMap() {
@@ -64,6 +64,18 @@ angular.module('myApp.map', ['ngRoute'])
       map.data.addListener('click', mapClickListener);
       initMapStyles();
     }
+
+    $scope.addPark = function (park) {
+      if (!$scope.parcs.selectedPark.subscribed) {
+        $scope.parcs.selectedPark.subscribed = true;
+        $scope.parcs.favs.push($scope.parcs.selectedPark);
+      }
+    }
+
+    $scope.removePark = function (id) {
+      $scope.parcs.favs = $scope.parcs.favs.filter(function (item) { return item.id !== id });
+    }
+
     function mapClickListener(event) {
 
       //cleanup
@@ -71,11 +83,19 @@ angular.module('myApp.map', ['ngRoute'])
       if (lastClickedFeature != undefined) lastClickedFeature.setProperty("clicked", false);
 
       //open new infowindow
+      $scope.parcs.selectedPark = event.feature.f;
+
       var infowindow = new google.maps.InfoWindow({
-        content: getInfoText(event.feature),
+        content: "<park-detail park='parcs.selectedPark' on-subscribe='addPark(park)' on-unsubscribe='removePark(id)' />",
         position: event.latLng
       });
       infowindow.open(map);
+
+      google.maps.event.addListener(infowindow, 'domready', function () {
+        $scope.$apply(function () { $compile($("#map-container").contents())($scope) });
+      });
+
+
       google.maps.event.addListener(infowindow, 'closeclick', function () {
         event.feature.setProperty("clicked", false);
       });
@@ -84,16 +104,6 @@ angular.module('myApp.map', ['ngRoute'])
       //remember currently opened infowindow for cleanup
       lastClickedFeature = event.feature;
       lastinfowindow = infowindow;
-    }
-
-    function getInfoText(feature) {
-      var text = infoWindowTemplate;
-
-      text = text.replace("%name%", feature.getProperty("name"))
-        .replace("%id%", feature.getProperty("@id"));
-
-      return text;
-
     }
 
 
@@ -121,190 +131,211 @@ angular.module('myApp.map', ['ngRoute'])
     }
 
     initMap();
-  }]);
+  }]).component('parkDetail', {
+    templateUrl: 'views/map/parkDetail.html',
+    controller: function () {
+      var ctrl = this;
 
-  
-    var mapStyle = [
+      ctrl.subscribe = function () {
+         ctrl.park.subscribed = true;
+        ctrl.onSubscribe({ park: ctrl.park });
+      };
+
+      ctrl.unsubscribe = function () {
+         ctrl.park.subscribed = false ;
+        ctrl.onUnsubscribe({ id: this.park.id });
+      };
+
+    },
+    bindings: {
+      park: '<',
+      onSubscribe: '&',
+      onUnsubscribe: '&'
+    }
+  });;
+
+
+var mapStyle = [
+  {
+    "elementType": "geometry",
+    "stylers": [
       {
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#f5f5f5"
-          }
-        ]
-      },
-      {
-        "elementType": "labels",
-        "stylers": [
-          {
-            "visibility": "off"
-          }
-        ]
-      },
-      {
-        "elementType": "labels.icon",
-        "stylers": [
-          {
-            "visibility": "off"
-          }
-        ]
-      },
-      {
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#616161"
-          }
-        ]
-      },
-      {
-        "elementType": "labels.text.stroke",
-        "stylers": [
-          {
-            "color": "#f5f5f5"
-          }
-        ]
-      },
-      {
-        "featureType": "administrative.land_parcel",
-        "stylers": [
-          {
-            "visibility": "off"
-          }
-        ]
-      },
-      {
-        "featureType": "administrative.land_parcel",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#bdbdbd"
-          }
-        ]
-      },
-      {
-        "featureType": "administrative.neighborhood",
-        "stylers": [
-          {
-            "visibility": "off"
-          }
-        ]
-      },
-      {
-        "featureType": "poi",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#eeeeee"
-          }
-        ]
-      },
-      {
-        "featureType": "poi",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#757575"
-          }
-        ]
-      },
-      {
-        "featureType": "poi.park",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#e5e5e5"
-          }
-        ]
-      },
-      {
-        "featureType": "poi.park",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#9e9e9e"
-          }
-        ]
-      },
-      {
-        "featureType": "road",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#ffffff"
-          }
-        ]
-      },
-      {
-        "featureType": "road.arterial",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#757575"
-          }
-        ]
-      },
-      {
-        "featureType": "road.highway",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#dadada"
-          }
-        ]
-      },
-      {
-        "featureType": "road.highway",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#616161"
-          }
-        ]
-      },
-      {
-        "featureType": "road.local",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#9e9e9e"
-          }
-        ]
-      },
-      {
-        "featureType": "transit.line",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#e5e5e5"
-          }
-        ]
-      },
-      {
-        "featureType": "transit.station",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#eeeeee"
-          }
-        ]
-      },
-      {
-        "featureType": "water",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#9eb9c7"
-          }
-        ]
-      },
-      {
-        "featureType": "water",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#9e9e9e"
-          }
-        ]
+        "color": "#f5f5f5"
       }
-    ];
+    ]
+  },
+  {
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#f5f5f5"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#bdbdbd"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.neighborhood",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#ffffff"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#dadada"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#9eb9c7"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  }
+];
